f7c2060e160bdf0bdfc115b49c6e8c1412c48524
shubham patel <shubhampatel9832@sageuniversity.in>
2024-12-18 19:06:11 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import Header from "./components/header";
 import Menu from "./components/menu";
 import Footer from "./components/footer";
 
+const appStyle = {
+    padding: "0 9rem",
+    backgroundColor: "#f7f2e9",
+    color: "#27272a",
+    paddingTop: "4rem",
+};
+
 function App() {
     // 1. Header
     // 2. Menu
     //    2.a. PizzaCard
     // 3. Footer
     return (
-        <div>
+        <div style={appStyle}>
             <Header />
             <Menu />
             <Footer />
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,34 @@
 import Pizza from "./Pizza";
+const menuBoxStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(2 , 1fr)",
+};
+
+const menuHeaderStyle = {
+    textTransform: "uppercase",
+    padding: "1rem 0",
+    borderTop: "3px solid #27272a",
+    borderBottom: "3px solid #27272a",
+    display: "inline-block",
+    margin: "0px auto",
+};
 
 function Menu() {
     return (
         <div>
             <div>
-                <h2> Our Menu </h2>
+                <h2 style={menuHeaderStyle}> Our Menu </h2>
                 <p>
                     Authentic Italin cuisine . 6 Creative to chose from. All
-                    from stone oven , all organic .
+                    from stone oven , all organic . Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Omnis aliquam quos eveniet
+                    corrupti amet temporibus blanditiis obcaecati dolore ad
+                    sapiente reiciendis quaerat qui velit atque esse eaque
+                    deserunt dolorum, quo expedita similique culpa.
                 </p>
             </div>
 
-            <div>
+            <div style={menuBoxStyle}>
                 <Pizza />
                 <Pizza />
                 <Pizza />
